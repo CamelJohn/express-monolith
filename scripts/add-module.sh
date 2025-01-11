8788b11fc010f6f9d36836a827d262c055d2516c
CamelJohn <jonathan23986@gmail.com>
2025-01-11 22:35:05 +0200

    update cp

--- scripts/add-module.sh
+++ scripts/add-module.sh
@@ -7,7 +7,7 @@ fi
 
 mkdir -p src/app/modules/$1
 
-cp -R templates/module/ src/app/modules/$1/
+cp -R $(pwd)/node_modules/bxp/templates/module/ src/app/modules/$1/
 
 echo "\nexport * from './$1';" >> src/app/modules/index.ts
 
